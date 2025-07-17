e0c8542968635e73ad62106b58f25a425cd3a183
Andrew Qu <qual1337@gmail.com>
2025-07-17 14:36:47 -0700

    init

---
+++ app/app.vue
@@ -0,0 +1,14 @@
+<script>
+import { initBotId } from 'botid/client'
+
+initBotId({
+  protect: [
+    {path: '/api/post-data', method: 'POST'}
+  ]
+})
+</script>
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
---
+++ app/pages/test.vue
@@ -0,0 +1,14 @@
+<script setup>
+const postData = () => {
+  fetch("/api/post-data", {
+    method: "POST",
+    body: JSON.stringify({ message: "Hello, world!" }),
+  });
+};
+</script>
+<template>
+  <div>
+    <div>Testing</div>
+    <button @click="postData">Post Data</button>
+  </div>
+</template>
